c6c5ad76c6f941c6c0494a0c59858930869dd73c
Simbidion <simonadwyer@yahoo.com.au>
2017-10-25 15:03:07 +1100

    trying to move method into helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -7,4 +7,31 @@ module BooksHelper
     end
     auth_arr.to_sentence
   end
+
+  # Rounds to nearest 0.5
+  def round_stars(average)
+    (average * 2).round / 2.0  
+  end
+
+  # Formats stars
+  def format_stars(book)
+    average_stars = book.reviews.average(:star_rating)
+    rounded_stars = round_stars(average_stars)
+
+    rounded_stars.floor.times do |index| 
+      fa_icon "star 2x", class: "book__star-rating" 
+    end 
+
+    if rounded_stars > rounded_stars.floor 
+      fa_icon "star-half-o 2x", class: "book__star-rating" 
+      (4 - average_stars.floor).times do |index| 
+        fa_icon "star-o 2x", class: "book__star-rating" 
+      end 
+    else 
+      (5 - rounded_stars.floor).times do |index| 
+        fa_icon "star-o 2x", class: "book__star-rating" 
+      end 
+    end 
+  end
+
 end
